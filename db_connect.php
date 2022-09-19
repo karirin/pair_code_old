58c0d506df08e636e01462cd426f8d6e24ece920
karirin <60878547+karirin@users.noreply.github.com>
2022-09-19 17:25:13 +0900

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -2,16 +2,16 @@
 function db_connect()
 {
   if ($_SERVER["HTTP_HOST"] == "localhost") {
-    $dsn = 'mysql:dbname=chat;host=localhost;charset=utf8';
+    $dsn = 'mysql:dbname=test;host=localhost;charset=utf8';
     $user = 'root';
     $password = '';
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbh;
   } else {
-    $dsn = 'mysql:dbname=heroku_30e4f1b3d09aef2;host=us-cdbr-east-05.cleardb.net;charset=utf8';
-    $user = 'bcf7f8bfec19ab';
-    $password = 'dfb3857b';
+    $dsn = 'mysql:dbname=heroku_585a8f1fe5ed8fb;host=us-cdbr-east-06.cleardb.net;charset=utf8';
+    $user = 'b18b6d1cf35115';
+    $password = 'da9e1fca';
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $dbh;
